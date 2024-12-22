8ca3d4923db17162498af12ea74216647918ad44
amanda2004 <amandasinggih17@gmail.com>
2024-12-22 23:27:34 +0700

    initial commit

--- src/services/loadHistoryData.js
+++ src/services/loadHistoryData.js
@@ -3,8 +3,8 @@ import { Firestore } from '@google-cloud/firestore';
 const loadHistoryData = async () => {
   const db = new Firestore({
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    projectId: 'submissionmlgc-raihanromzi',
-    databaseId: 'prediction',
+    projectId: 'submissionmlgc-amanda',
+    databaseId: 'predictions',
   });
 
   const predictCollection = db.collection('prediction');
--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,7 +3,7 @@ import { Firestore } from '@google-cloud/firestore';
 const storeData = async (id, data) => {
   const db = new Firestore({
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    projectId: 'submissionmlgc-tutorial',
+    projectId: 'submissionmlgc-amanda',
     databaseId: 'predictions',
   });
 
